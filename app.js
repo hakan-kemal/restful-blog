b797f437a74f8a1b832c6dda26eee9e3d43138ea
hakan-kemal <h.k.cuhadar@gmail.com>
2020-02-09 17:27:33 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/restful-blog', { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now() }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+// Blog.create({
+//     title: 'Just a day at work',
+//     image: 'https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
+//     body: 'Informal team meeting, we are just discussing the current state of our project and going through our todo\'s',
+// })
+
+app.get('/', (_req, res) => {
+    res.redirect('blogs');
+});
+
+app.get('/blogs', (_req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if (err) {
+            console.log(`Error: ${err}`);
+        } else {
+            res.render('index', { blogs: blogs });
+        }
+    })
+});
+
+app.get('*', (_req, res) => {
+    res.send('Page not found... this path doesn\'t exist');
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`🚀 Server ready at port :${port}`);
+});
